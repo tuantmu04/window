1616ac9a1e9c0d38e595fa9b9af59b9ec32df20a
Tuan Nguyen Dinh <92866918+tuantmu04@users.noreply.github.com>
2025-08-20 10:01:54 +0700

    Update install-windows-gcp.sh

--- install-windows-gcp.sh
+++ install-windows-gcp.sh
@@ -2,7 +2,7 @@
 
 # wget -qO- https://raw.githubusercontent.com/ngxson/public-assets/main/install-windows-gcp.sh | sudo bash
 
-IMAGE_URL="https://archive.org/download/windows4vpsisorecoverrrq11/windows12.gz"
+IMAGE_URL="https://fr1.teddyvps.com/iso/en_windows2012r2.gz"
 
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
